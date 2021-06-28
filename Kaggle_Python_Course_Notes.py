73acc66201c322e6cf9add0e071141ebe8588786
Kris Airdancer <chris.scott.marston@gmail.com>
2021-06-28 10:33:24 -0600

    Initial commit

---
+++ Kaggle_Python_Course_Notes.py
@@ -0,0 +1,9 @@
+help(round) # Provides info about the method whose name you pass as a parameter
+
+# Declaring a method in Python
+def myMethodName(parameters): # Don't forget the colon
+    return 1 + 2 # Return works the same in PY as in Java
+
+
+
+
