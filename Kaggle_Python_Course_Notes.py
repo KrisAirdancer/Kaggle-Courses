263f6441fdaf8515151d29e08b2df7c4b4ba7b0b
Kris Airdancer <chris.scott.marston@gmail.com>
2021-06-30 11:07:46 -0600

    Completed Kaggle's Python course

--- Kaggle_Python_Course_Notes.py
+++ Kaggle_Python_Course_Notes.py
@@ -626,6 +626,79 @@ print("it contains names such as...",
       dir(numpy.random)[-15:]
      )
 
+# Tips for working with strange/unknown stuff from imported libraries
+
+# Tip 1: Utilize type() - This function tells us what type of object something is.
+print(type(planet_to_initial))
+
+# Tip 2: Utilize dir() - This returns a list of attributes (variables) from an object
+dir()
+
+# mean() returns the average value from an object
+# toList() turns an array into a list
+
+# Tip 3: Utilize help()
+
+# Tip 4: Check the online docs for the function/library you are working with
+
+######################
+# Operator Overloading
+######################
+
+# Note: Arrays exist only in numpy, in vanilla Py they are called lists and they BEHAVE DIFFERENTLY!
+"""
+If one adds a value to a list, you get an error [10, 3, 5, 7, 8] + 10 -> ERROR
+If you do the same to a numpy array, you add the value to all of the elements
+in the array. [10, 3, 5, 7, 8] + 10 -> [20, 13, 15, 17, 18]
+"""
+
+"""
+Simply put, operator overloading is when an operator performs differently in
+different circumstances.
+"""
+
+# Here is a helpful way to change the format of tick-marks on graphs.
+"""
+ticks = graph.get_yticks()
+# Format those values into strings beginning with dollar sign
+new_labels = ['${}'.format(int(amt)) for amt in ticks]
+# Set the new labels
+graph.set_yticklabels(new_labels)
+"""
+
+# A helpful bit of example code
+"""
+def hand_total(hand):
+    # Helper function to calculate the total points of a blackjack hand.
+ 
+    total = 0
+    # Count the number of aces and deal with how to apply them at the end.
+    aces = 0
+    for card in hand:
+        if card in ['J', 'Q', 'K']:
+            total += 10
+        elif card == 'A':
+            aces += 1
+        else:
+            # Convert number cards (e.g. '7') to ints
+            total += int(card)
+    # At this point, total is the sum of this hand's cards *not counting aces*.
+
+    # Add aces, counting them as 1 for now. This is the smallest total we can make from this hand
+    total += aces
+    # "Upgrade" aces from 1 to 11 as long as it helps us get closer to 21
+    # without busting
+    while total + 10 <= 21 and aces > 0:
+        # Upgrade an ace from 1 to 11
+        total += 10
+        aces -= 1
+    return total
+
+def blackjack_hand_greater_than(hand_1, hand_2):
+    total_1 = hand_total(hand_1)
+    total_2 = hand_total(hand_2)
+    return total_1 <= 21 and (total_1 > total_2 or total_2 > 21)
+"""
 
 
 
