81da27788154ec43792e50e91142a9f0a3eb2162
Kris Airdancer <chris.scott.marston@gmail.com>
2021-06-30 13:17:44 -0600

    Completed Kaggle Pandas Modules 1 pts 1&2

--- Course Notes/Kaggle_Pandas_Course_Notes.py
+++ Course Notes/Kaggle_Pandas_Course_Notes.py
@@ -56,6 +56,43 @@ print(fruit_sales)
 # Series
 ########
 
+# A Series is a sequence of data values. If a DataFrame is a table, a Series is a list.
+# And in fact you can create one with nothing more than a list:
+print(pd.Series([1, 2, 3, 4, 5]))
+
+# A Series is, in essence, a single column of a DataFrame. So you can assign column
+# values to the Series the same way as before, using an index parameter. However, a
+# Series does not have a column name, it only has one overall name:
+
+print(pd.Series([30, 35, 40], index=['2015 Sales', '2016 Sales', '2017 Sales'], name='Product A'))
+
+# A DataFrame is essentially just a bunch of Series "glued" together.
+
+####################
+# Reading Data Files
+####################
+
+# CSV files are the most common type of data file.
+
+# pd.read_csv() can be used to read a CSV file into a DataFrame
+# newFrame = pd.read_csv(FILE DIRECTORY)
+
+# The .shape() function can be used to check the size of the DataFrame.
+# The output is (TOTAL NUMBER OF DATAPOINTS, NUMBER OF COLUMNS)	
+
+# The .head() function pulls the first 5 rows of data.
+# NOTE: The first row is numbered 0 - that is, the row index starts at 0 like everything else.
+
+"""
+The pd.read_csv() function is well-endowed, with over 30 optional parameters you can specify. 
+For example, you can see in this dataset that the CSV file has a built-in index, which pandas
+did not pick up on automatically. To make pandas use that column for the index (instead of
+creating a new one from scratch), we can specify an index_col.
+"""
+# wine_reviews = pd.read_csv("../input/wine-reviews/winemag-data-130k-v2.csv", index_col=0)
+# print(wine_reviews.head())
+
+# To write to a csv we can use, DataFrame.to_csv()
 
 
 
